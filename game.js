11a12df2e0a2ed152ad019dc784e1b22f71bd6d7
Himanshu <guptahimanshu479@gmail.com>
2019-09-29 10:16:26 +0530

    adding movement to the player

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ var canvasBg = document.getElementById("canvasBg"),
     canvasEntities = document.getElementById("canvasEntities"),
     ctxEntities = canvasEntities.getContext("2d"), //to define the context of the second canvas
     canvasWidth = canvasBg.width,
-    canvasHeight = canvasBg.clientHeight,
+    canvasHeight = canvasBg.height,
     player1 = new Player(),
     //enemies=[],
     //numEnemies=5,
@@ -23,8 +23,12 @@ imgSprite.src = "images/sprite.png";
 imgSprite.addEventListener("load", init, false); //loading the image, initialize it 
 
 function init() {
-    //document.addEventListener("keydown", checkKeyDown, false);
-    //document.addEventListener("keyup", checkKeyUp, false);
+    document.addEventListener("keydown", function(e){ checkKey(e, true);}, false); 
+    /*it checks the up key to move the player up
+    e is the event parameter passed to the function
+    true is passed as the second parameter*/
+   
+    document.addEventListener("keyup", function(e){ checkKey(e, false);} , false);
     //defineObstacles();
     //initEnemies();
     begin();    // it is used to begin the game and run it
@@ -76,11 +80,11 @@ function Player() {
     this.centerX = this.drawX + (this.width / 2);
     this.centerY = this.drawY + (this.height / 2); //where the center of the player is terms of the collisions
     this.speed = 2;//how fast the player will move
-    //this.isUpKey=false;
-    //this.isRightKey=false;
-    //this.IsDownKey=false;
-    //this.isLeftKey=false;
-    //this.isSpaceBar=false;
+    this.isUpKey=false;
+    this.isRightKey=false;
+    this.IsDownKey=false;
+    this.isLeftKey=false;
+    this.isSpaceBar=false;
     //this.isShooting=false;
     //var numBullets=10;
     //this.bullets=[];
@@ -94,12 +98,80 @@ Player.prototype.update = function () { //adding a method to the object
     this.centerX = this.drawX + (this.width / 2);
     this.centerY = this.drawY + (this.height / 2);
     //calculating the new centerX and centerY
-    //this.checkDirection();
+    this.checkDirection();
     //this.checkShooting();
     //this.updateAllBullets();
 };
 
-Player.prototype.draw = function () { //adding a method to the object
+Player.prototype.draw = function () { 
     //this.drawAllBullets();
     ctxEntities.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
 };
+
+Player.prototype.checkDirection = function () { 
+    var newDrawX=this.drawX,    
+        newDrawY=this.drawY,
+        obstacleCollision=false;
+        if(this.isUpKey){  //player upKey variable
+            newDrawY-=this.speed;
+            this.srcX=35; //Facing North player
+        }
+        else if(this.isDownKey){ 
+            newDrawY+=this.speed;
+            this.srcX=0; //Facing South
+        }
+        else if(this.isRightKey){ 
+            newDrawX+=this.speed;
+            this.srcX=105; //Facing East
+        }
+        else if(this.isLeftKey){  
+            newDrawX-=this.speed;
+            this.srcX=70; //Facing West
+        }
+        //NOTE: Here if we use if instead of else if, then we can move even indiagonal direction
+        //obstacleCollision =this.checkObstacleCollide(newDrawX, newDrawY);
+        if(!obstacleCollision&&!outOfBounds(this,newDrawX, newDrawY)){
+            this.drawX=newDrawX;
+            this.drawY=newDrawY;
+        }
+};
+
+function checkKey(e, value){
+    var keyID=e.keyCode || e.which; //check keyCode and if the browser doesnt support than we will check e.which
+    if(keyID===38){ //38 is the key for Up Arrow 
+        player1.isUpKey=value; 
+        e.preventDefault(); //it prevents the use of arrow keys to be used to scroll the page
+    }
+    if(keyID===39){ //Right Arrow 
+        player1.isRightKey=value; 
+        e.preventDefault(); 
+    }
+    if(keyID===40){ //Down Arrow 
+        player1.isDownKey=value; 
+        e.preventDefault(); 
+    }
+    if(keyID===37){ //Left Arrow 
+        player1.isLeftKey=value; 
+        e.preventDefault(); 
+    }
+    if(keyID===32){ //Space Bar 
+        player1.isSpaceBar =value; 
+        e.preventDefault(); 
+    }
+} 
+
+function outOfBounds(a,x,y){  //takes in object a, its position x and y as parameters
+    var newBottomY=y+a.height,
+        newTopY=y,
+        newRightX=x+a.width,
+        newLeftX=x,
+        treeLineTop=5,
+        treeLineBottom=570, 
+        treeLineRight=750,
+        treeLineLeft=65 //tree variables are the constraints that player cant go beyond that i.e. the boundary variables
+    
+    return newBottomY>treeLineBottom || 
+        newTopY<treeLineTop||
+        newRightX>treeLineRight||
+        newLeftX<treeLineLeft;
+}
